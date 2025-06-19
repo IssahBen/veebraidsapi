c9abf3d34ec2f1564103c5bb4f044646da745111
IssahBen <benedictissah@gmail.com>
2025-06-18 22:48:25 -0400

    signup

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -5,7 +5,6 @@ module Api
         user = User.new(user_params)
 
         if user.save
-          UserMailer.welcome_email(user).deliver_now
           render json: { token: user.authentication_token, user: user.as_json }, status: :ok
         else
           render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
@@ -18,7 +17,7 @@ module Api
     end
 
     def user_params
-      params.require(:user).permit(:email, :password, :password_confirmation, :name)
+      params.require(:user).permit(:email, :password, :password_confirmation)
     end
   end
 end
